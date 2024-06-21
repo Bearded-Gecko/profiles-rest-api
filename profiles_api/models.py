3fdc2c037b467fc85f16d3c82a16adc2e44a88ca
Mike_Ho_G15 <michaelyho1@gmail.com>
2024-06-20 22:33:28 -0700

    Added custom user profile model, manager and migrations

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,55 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser #needed to customize/overwrite the default user django model
+from django.contrib.auth.models import PermissionsMixin #needed to customize/overwrite the default user django model
+from django.contrib.auth.models import BaseUserManager
+
+
+class UserProfileManager(BaseUserManager): #so django knows how to work with the custom userprofile class
+    """Manager for user profiles"""
+
+    def create_user(self, email, name, password=None):
+        """Create a new user profile"""
+        if not email:
+            raise ValueError('User must have an email address') #make sure proper email address value has been passed in
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+        user.set_password(password) #we use set_password to encrypt it, it is converted to hash and not in plain text
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, name, password):
+        """Create and save a new superuser with given details"""
+        user = self.create_user(email, name, password)
+
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
 
 # Create your models here.
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """Database model for users in the system"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserProfileManager() #custom profile manager
+
+    USERNAME_FIELD = 'email' #to customize default username in django; field is required by default
+    REQUIRED_FIELDS = ['name'] #additional required fields
+
+    def get_full_name(self):
+        """Retrieve full name of user"""
+        return self.name
+
+    def get_short_name(self):
+        """Retrive short name of user"""
+        return self.name
+
+    def __str__(self):
+        """Return string representation of our user"""
+        return self.email
